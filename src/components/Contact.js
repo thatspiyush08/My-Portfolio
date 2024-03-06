c2c620b7369d27945a096b9c20ef85d2e2c51cc5
thatspiyush08 <piyushnaula008@gmail.com>
2024-03-06 20:46:37 +0530

    added contact me

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { motion } from 'framer-motion';
+import { fadeIn } from '../variants';
+
+
 
 const Contact = () => {
-  return <div className='section'id ='contact'>Contact</div>;
+  return <section className='py-16 lg:section'id ='contact'>
+    <div className='container mx-auto'>
+
+      <div className='flex flex-col lg:flex-row'>
+        <motion.div
+        variants={fadeIn('right',0.4)} initial='hidden' whileInView={'show'} viewport={{once:false,amount:0.7}} className='flex-1 flex justify-start items-center'>
+          <div>
+              <h4 className='etxt-x1 uppercase text-accent font-medium mb-2 tracking-wide'>Get in Touch</h4>
+              <h2 className='text-[45px] lg:text-[90px] leading-none mb-12'>Let's work <br/> together!!</h2>
+          </div>
+        </motion.div>
+        <motion.form variants={fadeIn('left',0.6)} initial='hidden' whileInView={'show'} viewport={{once:false,amount:0.7}}
+         className='flex-1 border rounded-2xl flex flex-col gap-y-6 pb-24 p-6 items-start'>
+          <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white
+          focus:border-accent transition-all' type='text' placeholder='Your Name'></input>
+        
+
+        
+          <input className='bg-transparent border-b py-3 outline-none w-full placeholder:text-white
+          focus:border-accent transition-all' type='text' placeholder='Your Email'></input>
+
+
+        <textarea className=' bg-transparent border-b py-3 outline-none w-full placeholder:text-white focus:border-accent transition-all resize-none mb-12'
+         type='text' placeholder='Your Message'></textarea>
+         <button className='btn btn-lg'>Send Message</button>
+         </motion.form>
+      </div>
+
+    </div>
+  </section>;
 };
 
 export default Contact;
