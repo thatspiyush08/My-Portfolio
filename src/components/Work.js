234149bd1cf6403caf8a51eee1f51eb9be1ed1a7
thatspiyush08 <piyushnaula008@gmail.com>
2024-03-07 18:54:19 +0530

    added motion

--- src/components/Work.js
+++ src/components/Work.js
@@ -11,7 +11,8 @@ const Work = () => {
     <div className='container mx-auto'>
 
       <div className='flex flex-col lg:flex-row gap-x-10'>
-        <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+        <motion.div  variants={fadeIn('right',0.3)} initial='hidden' whileInView={'show'} viewport={{once:false,amount:0.7}} 
+         className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
 
           <div>
             
@@ -35,8 +36,9 @@ const Work = () => {
             </div>
             </div>
 
-        </div>
-        <div className='flex-1 flex flex-col gap-y-10'>
+        </motion.div>
+        <motion.div  variants={fadeIn('left',0.2)} initial='hidden' whileInView={'show'} viewport={{once:false,amount:0.7}}
+        className='flex-1 flex flex-col gap-y-10'>
         <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
             <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
             <img className='group-hover:scale-125 transition-all duration-500' src={Img2} alt="Project Image" />
@@ -59,7 +61,7 @@ const Work = () => {
             </div>
             
         </div>
-        </div>
+        </motion.div>
         
       </div>
       
