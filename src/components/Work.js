93e836e52220e52d128cb305eb67f09d56fecbee
thatspiyush08 <piyushnaula008@gmail.com>
2024-03-07 21:03:04 +0530

    corrected link

--- src/components/Work.js
+++ src/components/Work.js
@@ -28,8 +28,9 @@ const Work = () => {
             </a>
             
           </div>
-          <a href='https://real-time-crypto-price.vercel.app/'>
+          
           <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+          <a href='https://real-time-crypto-price.vercel.app/'>
             <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
             <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt="Project Image" style={{ width: '600px', height: '320px' }} />
             <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
@@ -38,8 +39,9 @@ const Work = () => {
             <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
               <span className='text-3xl text-white'>Real Time Crypto Price Teller</span>
             </div>
+            </a>
             </div>
-          </a>
+          
             
 
         </motion.div>
