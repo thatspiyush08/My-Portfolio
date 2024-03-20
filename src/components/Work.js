51fd0dcca7763da994fa56d5c6ddb4cbfc84d91a
thatspiyush08 <piyushnaula008@gmail.com>
2024-03-20 20:25:56 +0530

    Added Work

--- src/components/Work.js
+++ src/components/Work.js
@@ -48,14 +48,14 @@ const Work = () => {
         <motion.div  variants={fadeIn('left',0.2)} initial='hidden' whileInView={'show'} viewport={{once:false,amount:0.7}}
         className='flex-1 flex flex-col gap-y-8'>
         <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
-          <a href='https://todo-react-blush-seven.vercel.app/'>
+          <a href='https://audiobytes.vercel.app/'>
           <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
             <img className='group-hover:scale-125 transition-all duration-500' src={Img2} style={{ width: '600px', height: '350px' }}alt="Project Image" />
             <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
               <span className='text-gradient'>Project 2</span>
             </div>
             <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-              <span className='text-3xl text-white'>Todo Application Using React</span>
+              <span className='text-3xl text-white'>Official AudioBytes Website</span>
             </div> </a>
             
             
